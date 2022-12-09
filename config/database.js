e6a7c2bde0aaa68d3b0e0eba0c10b7eb11b0cace
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2022-12-09 05:53:24 +0100

    configs

--- config/database.js
+++ config/database.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
-      host: env('DATABASE_HOST', '0.0.0.0'),
+      host: env('DATABASE_HOST', 'admin-esic-online-db'),
       //port: env('DATABASE_PORT', 5432),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'postgres'),
-      database: env('DATABASE_NAME', 'db-esic-online')
+      database: env('DATABASE_NAME', 'admin-esic-online')
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
