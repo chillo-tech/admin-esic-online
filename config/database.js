2ebec9df4f471f8154389457b22a867a253f9a60
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2022-12-09 07:08:55 +0100

    configurations db

--- config/database.js
+++ config/database.js
@@ -9,11 +9,8 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
+      schema: env('DATABASE_SCHEMA', 'public'), 
     },
-    debug: false,
+    debug: true,
   },
 });
