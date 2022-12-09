ebf0af0091fd4fbbce758f9e444a8846b6001288
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2022-12-09 07:12:41 +0100

    configurations db

--- config/database.js
+++ config/database.js
@@ -1,16 +1,14 @@
-// path: ./config/database.js
-
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), 
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "bank"),
+      username: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "0000"),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-    debug: true,
-  },
+  }
 });
