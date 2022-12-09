254feef40d7a9391f605d37d2119d1519a176d94
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2022-12-09 06:09:37 +0100

    configs

--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
-const path = require('path');
+// path: ./config/database.js
 
 module.exports = ({ env }) => ({
   connection: {
-    client: env("DATABASE_CLIENT", "postgres"),
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'admin-esic-online-db'),
-      //port: env('DATABASE_PORT', 5432),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
-      database: env('DATABASE_NAME', 'admin-esic-online')
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
     debug: false,
   },
